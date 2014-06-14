5be6f5e047de203dec5c69bb9b7a6718a70b8fc8
tknew2 <strzelewicz.alexandre@gmail.com>
2014-06-14 13:42:45 +0200

    psh

---
+++ deploy.js
@@ -0,0 +1,20 @@
+
+var fs    = require('fs');
+var spawn = require('child_process').spawn;
+
+function deployForEnv(conf_file, env, args) {
+  var conf    = fs.readFileSync(conf_file);
+  var outData = JSON.stringify(JSON.parse(conf)['deploy'][env]);
+
+  var shellSyntaxCommand = "echo '" + outData + "' | ./deploy " + args;
+  spawn('sh', ['-c', shellSyntaxCommand], { stdio: 'inherit' });
+}
+
+var args = process.argv;
+
+if (args.indexOf('deploy') == -1)
+  throw new Error('deploy argument not found');
+
+args.splice(0, args.indexOf('deploy') + 1);
+
+deployForEnv('app.json', 'production', args);
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git add .; git commit -am "psh"; git push
-ssh strzel_a@steelsecurity.co "cd /home/strzel_a/vitrine && git pull"
