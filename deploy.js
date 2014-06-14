e8466835b3da5ae8477149d2a73d859352d9feb4
tknew2 <strzelewicz.alexandre@gmail.com>
2014-06-14 13:44:13 +0200

    psh

--- deploy.js
+++ deploy.js
@@ -6,6 +6,7 @@ function deployForEnv(conf_file, env, args) {
   var conf    = fs.readFileSync(conf_file);
   var outData = JSON.stringify(JSON.parse(conf)['deploy'][env]);
 
+  console.log(arguments);
   var shellSyntaxCommand = "echo '" + outData + "' | ./deploy " + args;
   spawn('sh', ['-c', shellSyntaxCommand], { stdio: 'inherit' });
 }
@@ -17,4 +18,4 @@ if (args.indexOf('deploy') == -1)
 
 args.splice(0, args.indexOf('deploy') + 1);
 
-deployForEnv('app.json', args[1], args);
+deployForEnv('app.json', args[0], args);
